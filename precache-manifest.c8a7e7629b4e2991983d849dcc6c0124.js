94846f53728ad2a861bc0de22c0f9c943dc630a9
surya0077 <63136853+surya0077@users.noreply.github.com>
2020-05-18 22:27:27 +0530

    Updates

---
+++ precache-manifest.c8a7e7629b4e2991983d849dcc6c0124.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1cfee074c424f0123bbdc35e269bc8",
+    "url": "/RoboFreinds/index.html"
+  },
+  {
+    "revision": "6685e56cff6a5a268409",
+    "url": "/RoboFreinds/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7b21b4cffceafee54860",
+    "url": "/RoboFreinds/static/css/main.ef280af6.chunk.css"
+  },
+  {
+    "revision": "6685e56cff6a5a268409",
+    "url": "/RoboFreinds/static/js/2.ffbaab67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFreinds/static/js/2.ffbaab67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b21b4cffceafee54860",
+    "url": "/RoboFreinds/static/js/main.fca39ffc.chunk.js"
+  },
+  {
+    "revision": "a79e8a59cf206253a229",
+    "url": "/RoboFreinds/static/js/runtime-main.299b9b1a.js"
+  },
+  {
+    "revision": "769b6c3bdb11cd0ed5391c808539f6a6",
+    "url": "/RoboFreinds/static/media/WebPressBold.769b6c3b.woff"
+  }
+]);
